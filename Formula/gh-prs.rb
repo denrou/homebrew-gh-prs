1223a7f35a9961029f2211001f729567756c2c42
Denis Roussel <droussel@centreon.com>
2026-07-27 10:49:05 -0400

    Modernize formula generation: update-python-resources, add 0.7.0 formula (#1)
    Replace the unmaintained homebrew-pypi-poet (which imports the removed
    pkg_resources and broke on Python 3.14) with Homebrew's built-in
    brew update-python-resources, using --ignore-main-package-cooldown so it
    works the same day as a release. Commit the generated, install-verified
    gh-prs 0.7.0 formula.

---
+++ Formula/gh-prs.rb
@@ -0,0 +1,39 @@
+class GhPrs < Formula
+  include Language::Python::Virtualenv
+
+  desc "CLI for listing GitHub pull requests that need your attention"
+  homepage "https://github.com/denrou/gh-prs"
+  url "https://files.pythonhosted.org/packages/79/f8/5e34fceb7337233041e3046bbc82bf2f33df890d390b806893513694318d/gh_prs-0.7.0.tar.gz"
+  sha256 "057dc376486010c3d01e4af2026c974e184f03e98b2fe74fd2a956b59cca6c3d"
+  license "MIT"
+
+  depends_on "python@3.14"
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/06/ff/7841249c247aa650a76b9ee4bbaeae59370dc8bfd2f6c01f3630c35eb134/markdown_it_py-4.2.0.tar.gz"
+    sha256 "04a21681d6fbb623de53f6f364d352309d4094dd4194040a10fd51833e418d49"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/c3/b2/bc9c9196916376152d655522fdcebac55e66de6603a76a02bca1b6414f6c/pygments-2.20.0.tar.gz"
+    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/c0/8f/0722ca900cc807c13a6a0c696dacf35430f72e0ec571c4275d2371fca3e9/rich-15.0.0.tar.gz"
+    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gh-prs --version")
+  end
+end
